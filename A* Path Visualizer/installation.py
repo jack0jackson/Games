33fd5c6c9d874ada75d110f2f43c827f7a84decd
ok987654321 <116027569+ok987654321@users.noreply.github.com>
2022-10-25 12:55:43 +0530

    Update installation.py

--- A* Path Visualizer/installation.py
+++ A* Path Visualizer/installation.py
@@ -1,28 +1,46 @@
 import subprocess
+
 import sys
+
 import get_pip
+
 import os
+
 import importlib
+
 import contextlib
 
+
+
 def install(package):
+
     '''
+    
     installs a package using pip
 
-    :param package: string
-    '''
+
+
+:param package: string
+
+'''
+
     subprocess.call([sys.executable, "-m", "pip", "install", package])
 
+    
 
 required = []
+
 failed = []
 
+
 # Try to open reqirements.txt file and read all required packages
+
 try:
     file = open("requirements.txt", "r")
     file_lines = file.readlines()
     required = [line.strip().lower() for line in file_lines]
     file.close()
+
 except FileNotFoundError:
     print("[ERROR] No requiremnts.txt file found")
 
